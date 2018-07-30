1716ce0d8a29f5faa12cce0ff0a5c6394c203e5c
Ben Jamin <Ben Jamin>
2018-07-30 15:07:54 +0100

    Added version checking for jitsi-meet-tokens

--- patch_jitsi-meet-tokens_for_kiwi.sh
+++ patch_jitsi-meet-tokens_for_kiwi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MINVER=1.0.2942-1
+
 echo "Kiwi patcher for Jitsi Meet Tokens"
 echo ""
 
@@ -12,6 +14,26 @@ if [ ! -d "/usr/share/jitsi-meet/prosody-plugins/" ] ; then
 	echo "error: /usr/share/jitsi-meet/prosody-plugins/ not found!"
 	exit 1
 fi
+
+if [ ! `dpkg -l jitsi-meet-tokens | grep ii | wc -l` = 1 ] ; then
+	echo "error: requires jitsi-meet-tokens package version >= 1.0.2942 and < 1.1.0"
+	exit 1
+fi
+
+TVER=`dpkg -s jitsi-meet-tokens | grep Version | cut -f 2 -d " "`
+
+dpkg --compare-versions $MINVER le $TVER
+if [ $? = 1 ] ; then
+	echo "error: requires jitsi-meet-tokens package version >= 1.0.2942 and < 1.1.0"
+	exit 1
+fi
+
+dpkg --compare-versions 1.1.0 gt $TVER
+if [ $? = 1 ] ; then
+	echo "error: requires jitsi-meet-tokens package version >= 1.0.2942 and < 1.1.0"
+	exit 1
+fi
+
 
 echo "Applying kiwiirc-jitsi-meet-tokens.patch to /usr/share/jitsi-meet/prosody-plugins/"
 echo "..."
